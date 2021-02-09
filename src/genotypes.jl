a0485b5dfea696b5dd7a8e6f3e2c1c20f5663202
Xijiang Yu <xijiang@users.noreply.github.com>
2021-02-09 13:36:48 +0100

    simplified gt O

--- src/genotypes.jl
+++ src/genotypes.jl
@@ -7,7 +7,7 @@ names in two separate files for other programs.
 The result files are `target.id` and `target.gt`.
 """
 function output_gt(target, src...)
-    title("Compute G with $src")
+    title("Output G with $src")
     item("Check if files available and qualified")
     nlc = Int[]
     for i in src
@@ -35,31 +35,24 @@ function output_gt(target, src...)
     tmp = mktempdir(".")
     write("$tmp/merge.lst", join(src, '\n'), '\n')
     run(`$plink --sheep --merge-list $tmp/merge.lst --out $tmp/plink`)
-    ID = begin
-        tv = String[]
+    open("$target.id", "w") do io
         for line in eachline("$tmp/plink.fam")
-            id = split(line)[2]
-            push!(tv, id)
+            println(io, split(line)[2])
         end
-        tv
     end
     run(`$plink --sheep --bfile $tmp/plink --recode A --out $tmp/plink`)
     open("$tmp/plink.raw", "r") do io
         _  = readline(io)       # skip the line of SNP names
-        fid = open("$target.id", "w")
-        nid = 0
         buffer = IOBuffer()
+        nid = 0
         for line in eachline(io)
             nid += 1
-            t = findnext(' ', line, 1) - 1
-            println(fid, line[1:t])
-            t += 2
-            for _ in 2:6
+            t = 1
+            for _ in 1:6
                 t = findnext(' ', line, t) + 1
             end
             write(buffer, replace(line[t:end], ' '=>""))
         end
-        close(fid)
         open("$target.gt", "w") do gt
             GT = reshape(take!(buffer), nid, :)
             for i in 1:size(GT)[1]
